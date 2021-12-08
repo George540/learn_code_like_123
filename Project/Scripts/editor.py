dc847ff9a7354c4207bf3f45b62aba19c2c97320
anh <trinhphuonganh99@gmail.com>
2021-12-07 19:22:29 -0500

    Added comments in editor.py

--- Project/Scripts/editor.py
+++ Project/Scripts/editor.py
@@ -19,10 +19,13 @@ class TextEditor:
 	def __init__(self, root):
 		# Assigning root
 		self.root = root
-
+		# Assigning window width
 		self.windowWidth = 1200
+		# Assigning window height
 		self.windowHeight = 800
+		# Assigning geometry for the root
 		self.root.geometry(str(self.windowWidth)+'x'+str(self.windowHeight))
+		# Assigning current sentence
 		self.current_sentence = ""
 
 		# Title of the window
@@ -31,42 +34,52 @@ class TextEditor:
 		self.filename = None
 		# Declaring Title variable
 		self.title = StringVar()
-
+		# List of Logs
 		self.Logs = []
+		# List of sentence logs
 		self.sentenceLogs = []
+		# List of result logs
 		self.resultLogs = []
+		# List of input sentences
 		self.listSentences = []
+		# List of result 
 		self.listResults = []
+		# Declare list of sentences variable
 		self.listSentencesVar = StringVar(value=self.listSentences)
+		# Declare list of results variable
 		self.listResultsVar = StringVar(value=self.listResults)
+		# Initialize sentence index
 		self.sentenceIndex = 0
 
 		# --------------------------------------------------------------------------------
 		# --------------------------------------------------------------------------------
-
-		# Set color and other properties
+		# Set color and other properties of the text editor
 		self.titlebar = Label(self.root, textvariable=self.title, font=(
-			"Roboto", 12, "bold"), background="#e9ecef")  # Creating Titlebar
+			"Roboto", 12, "bold"), background="#e9ecef")  
 		self.body = Frame(self.root, background="#fff")
+		
 		self.sentences = Frame(self.body, background="#e9ecef")
 		self.results = Frame(self.body, background="#ced4da")
+		
 		self.sentenceInput = Frame(self.sentences, background="#ced4da")
 		self.sentenceArea = Frame(self.sentences, background="#e9ecef")
 		self.sentenceRun = Frame(self.sentences, background="#e9ecef")
+		
 		self.sentenceInputBoxLengthVar = StringVar()
 		self.sentenceInputBoxLengthVar.trace('w', self.limitCharacterAmount)
 		self.sentenceInputBox = Entry(self.sentenceInput, font=(
 			"Roboto", 12), bg="#e9ecef", relief="flat", textvariable=self.sentenceInputBoxLengthVar)
+		
 		self.resultsHeading = Frame(
 			self.results, background="#ced4da", height=40)
-
 		self.sentenceInputBox.bind('<Return>', self.append_to_SentenceArea)
-
 		self.resultsHeadingTextVariable = StringVar()
+		
 		self.resultsHeadingText = Label(
 			self.resultsHeading, textvariable=self.resultsHeadingTextVariable, font=(
 				"Roboto", 12, "bold"), anchor="w", background="#ced4da", fg="#7A7A7A")
 		self.resultsHeadingTextVariable.set("Results")
+		
 		self.resultOutputs = Frame(self.results, background="#ced4da")
 
 		# Creating Scrollbar for sentence area
@@ -74,11 +87,10 @@ class TextEditor:
 			self.sentenceArea, orient=VERTICAL)
 		self.sentenceAreaBox = Listbox(
 			self.sentenceArea, listvariable=self.listSentencesVar, yscrollcommand=self.sentenceScrollbar.set, background="#e9ecef", bd=0, highlightcolor="#e9ecef", selectbackground="#7A7A7A", font=("Roboto", 14), activestyle='none', exportselection=False, selectmode=SINGLE)
-
 		self.sentenceScrollbar.config(command=self.sentenceAreaBox.yview)
-
 		self.sentenceAreaBox.bind("<<ListboxSelect>>", self.doSelection)
-		# registering the observer
+
+		# Registering the observer
 		self.listSentencesVar.trace_add('write', self.takeSentenceLogs)
 
 		# Creating Scrollbar for resultOutputs
@@ -86,18 +98,16 @@ class TextEditor:
 			self.resultOutputs, orient=VERTICAL)
 		self.resultsAreaBox = Listbox(
 			self.resultOutputs, listvariable=self.listResultsVar, yscrollcommand=self.resultsScrollbar.set, background="#ced4da", bd=0, highlightcolor="#e9ecef", selectbackground="#7A7A7A", font=("Roboto", 14), activestyle='none', exportselection=False, selectmode=SINGLE)
-
 		self.resultsScrollbar.config(command=self.resultsAreaBox.yview)
-
 		self.resultsAreaBox.bind("<<ListboxSelect>>", self.doSelectionResults)
 
+		# Registering the observer
 		self.listResultsVar.trace_add('write', self.takeResultsLogs)
 
 		# Create Run button
 		self.setButton()
 
 		# --------------------------------------------------------------------------------
-
 		# Assign component locations
 		self.titlebar.grid(row=0, column=0, sticky="nsew")
 		self.body.grid(row=1, column=0, sticky="nsew")
@@ -154,13 +164,10 @@ class TextEditor:
 		self.resultOutputs.grid_rowconfigure(0, weight=1)
 
 		# --------------------------------------------------------------------------------
-		# --------------------------------------------------------------------------------
-		# header layout
-
+		# Header layout
 		# Creating Menubar
 		self.menubar = Menu(self.root, font=("times new roman",
 											 15, "bold"), activebackground="skyblue")
-
 		# Configuring menubar on header
 		self.root.config(menu=self.menubar)
 
@@ -172,23 +179,10 @@ class TextEditor:
 		# Creating File Menu
 		self.filemenu = Menu(self.menubar, font=(
 			"Roboto", 11), activebackground="skyblue", tearoff=0)
-		# # Adding New file Command
-		# self.filemenu.add_command(
-		#     label="New", accelerator="Ctrl+N", command=self.newfile)
-		# # Adding Open file Command
-		# self.filemenu.add_command(
-		#     label="Open", accelerator="Ctrl+O", command=self.openfile)
-		# # Adding Save File Command
-		# self.filemenu.add_command(
-		#     label="Save", accelerator="Ctrl+S", command=self.savefile)
-		# # Adding Save As file Command
-		# self.filemenu.add_command(
-		#     label="Save logs to file", accelerator="Ctrl+A", command=self.saveasfile)
 
 		# Adding Save As file Command
 		self.filemenu.add_command(
 			label="Save Logs to File", accelerator="Ctrl+S", command=self.saveasfile)
-
 		# Adding Seprator
 		self.filemenu.add_separator()
 		# Adding Exit window Command
@@ -210,11 +204,6 @@ class TextEditor:
 		# Adding Paste text command
 		self.editmenu.add_command(
 			label="Paste", accelerator="Ctrl+V", command=self.paste)
-		# Adding Seprator
-		# self.editmenu.add_separator()
-		# Adding Undo text Command
-		# self.editmenu.add_command(
-		#     label="Undo", accelerator="Ctrl+U", command=self.undo)
 		# Cascading editmenu to menubar
 		self.menubar.add_cascade(label="Edit", menu=self.editmenu)
 
@@ -232,7 +221,6 @@ class TextEditor:
 		self.menubar.add_cascade(label="Help", menu=self.helpmenu)
 
 		# --------------------------------------------------------------------------------
-
 		# Calling shortcuts function
 		self.shortcuts()
 
@@ -317,15 +305,13 @@ class TextEditor:
 			untitledfile = filedialog.asksaveasfilename(title="Save file As", defaultextension=".txt", initialfile="Untitled.txt", filetypes=(
 				("All Files", "*.*"), ("Text Files", "*.txt"), ("Python Files", "*.py")))
 			# Reading the data from text area
-
 			for i in range(len(self.sentenceLogs)):
 				row = " --> ".join([self.sentenceLogs[i], self.resultLogs[i]])
 				if row.split()[0] != "-->":
 					self.Logs.append(row)
-
 			data = "Input --> Output \n"
+			# Join data
 			data += "\n".join(self.Logs)
-
 			# opening File in write mode
 			outfile = open(untitledfile, "w")
 			# Writing Data into file
@@ -336,8 +322,6 @@ class TextEditor:
 			self.filename = untitledfile
 			# Calling Set title
 			self.settitle()
-			# Updating Status
-			# self.status.set("Saved Successfully")
 		except Exception as e:
 			messagebox.showerror("Exception", e)
 
@@ -361,36 +345,6 @@ class TextEditor:
 	def paste(self, *args):
 		self.sentenceInputBox.event_generate("<<Paste>>")
 
-	# Defining Undo Funtion
-	def undo(self, *args):
-		# Exception handling
-		try:
-			# checking if filename not none
-			if self.filename:
-				# Clearing Text Area
-				self.sentenceInputBox.delete()
-				# opening File in read mode
-				infile = open(self.filename, "r")
-				# Inserting data Line by line into text area
-				for line in infile:
-					self.sentenceInputBox.insert(END, line)
-				# Closing File
-				infile.close()
-				# Calling Set title
-				self.settitle()
-				# Updating Status
-				self.status.set("Undone Successfully")
-			else:
-				# Clearing Text Area
-				self.sentenceInputBox.delete()
-				# Updating filename as None
-				self.filename = None
-				# Calling Set title
-				self.settitle()
-				# Updating Status
-				self.status.set("Undone Successfully")
-		except Exception as e:
-			messagebox.showerror("Exception", e)
 	# Defining Dictionary Function
 	def infodictionary(self):
 		messagebox.showinfo("Reserved Keywords", "Let...be...\nAdd...to...\nAdd...and...\nSubstract...from...\n" +
@@ -401,14 +355,8 @@ class TextEditor:
 
 	# Defining shortcuts Funtion
 	def shortcuts(self):
-		# Binding Ctrl+n to newfile funtion
-		self.sentenceInputBox.bind("<Control-n>", self.newfile)
-		# Binding Ctrl+o to openfile funtion
-		self.sentenceInputBox.bind("<Control-o>", self.openfile)
 		# Binding Ctrl+s to savefile funtion
 		self.sentenceInputBox.bind("<Control-s>", self.saveasfile)
-		# Binding Ctrl+a to saveasfile funtion
-		# self.sentenceInputBox.bind("<Control-a>", self.saveasfile)
 		# Binding Ctrl+e to exit funtion
 		self.sentenceInputBox.bind("<Control-e>", self.exit)
 		# Binding Ctrl+x to cut funtion
@@ -417,8 +365,6 @@ class TextEditor:
 		self.sentenceInputBox.bind("<Control-c>", self.copy)
 		# Binding Ctrl+v to paste funtion
 		self.sentenceInputBox.bind("<Control-v>", self.paste)
-		# Binding Ctrl+u to undo funtion
-		self.sentenceInputBox.bind("<Control-u>", self.undo)
 
 	def limitCharacterAmount(self, *args):
 		limit = 60  # Max amount of characters allowed in input text box
@@ -426,12 +372,14 @@ class TextEditor:
 		if len(characters) > limit:
 			self.sentenceInputBoxLengthVar.set(characters[:limit])
 
+	# Define Run button
 	def setButton(self):
 		self.runphoto = PhotoImage(file=r"Images/run_button.png")
 		self.runButton = Button(
 			self.sentenceRun, image=self.runphoto, command=self.append_to_SentenceArea, background="#ced4da", height=30, width=70, relief="flat", bd=0, activebackground="#ced4da")
 		return self.current_sentence
 
+	# History of the inputted sentences will be appended to sentence area
 	def append_to_SentenceArea(self, event=""):
 		if self.sentenceInputBox.get():
 			sentence = " ".join(self.sentenceInputBox.get().split())
@@ -440,11 +388,12 @@ class TextEditor:
 			self.clear_entry_text()
 			self.process_Algorithm(sentence)
 
+	# Define function to empty entry box when "Enter" or "Run" button is hit
 	def clear_entry_text(self):
 		self.sentenceInputBox.delete(0, END)
 
+	# Define function to pass the written algorithm to process input sentence
 	def process_Algorithm(self, sentence):
-    	# splits input sentence into clauses
 		sentence_interpreter.setCurrentSentence(sentence)
 		sentence_interpreter.splitSentense()
 
@@ -452,9 +401,7 @@ class TextEditor:
 		total_clauses_result = []
 		isBroken = False
 		for i, c in enumerate(sentence_interpreter.getCurrentClauses()):
-			# splits clause into a list of words
 			current_words = sentence_interpreter.splitClause(c)
-
 			# Check if condition is not followed by a statement, return error
 			# This avoids any unecessary processes afterwards
 			if current_words[0].lower() == "if" and current_words[2] == "is" and i + 1 >= len(sentence_interpreter.getCurrentClauses()):
@@ -465,29 +412,26 @@ class TextEditor:
 				isBroken = True
 				break
 
-			# Match with correct algorithm, if any
 			algorithm_finder.setCurr_words(current_words)
 			algorithm_finder.resetValues()
 			algorithm_finder.findAlgorithm()
 
-			# append clause result into list
 			total_clauses_result.append(algorithm_finder.getResult())
 
-			# if the clause is a condition and is false, skip entire next clause, if any
 			clausesPassed += 1
 			if (algorithm_finder.getResult() == "False condition. Statement skipped" or algorithm_finder.getResult() == "Format Error: Try a new sentence"):
 				break
 
-		# If sentence was not broken due to no statement after condition,
-		# put all results next to each other
 		if isBroken == False:
 			self.append_to_ResultsArea("")
 			self.append_to_ResultsArea(
 				', '.join(map(str, total_clauses_result)))
 
+	# Define a function to append the result to results area after proccessing the algorithm
 	def append_to_ResultsArea(self, output):
 		self.resultsAreaBox.insert(0, output)
 
+	# Define functions to highlight the history of an input sentence and its corresponding result
 	def doSelection(self, event):
 		w = event.widget
 		selected_index = w.curselection()
@@ -510,6 +454,7 @@ class TextEditor:
 				self.sentenceAreaBox.selection_clear(0, END)
 				self.sentenceAreaBox.selection_set(selected_index)
 
+	# Define functions to callback when adding observer
 	def takeSentenceLogs(self, var, indx, mode):
 		sentence = self.listSentencesVar.get().replace("(", "")
 		sentence = sentence.replace(")", "")
